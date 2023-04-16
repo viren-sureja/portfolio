85f25a1d2828eae8c03e574ee060317221546ea0
viren sureja <virensureja3@gmail.com>
2023-04-17 01:19:04 +0530

    Skills show hide features added

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close');
-console.log(navClose);
 
 // Logic to show menu
 if (navToggle) {
@@ -22,7 +21,28 @@ const navLink = document.querySelectorAll('.nav__link');
 function linkAction() {
     const navMenu = document.getElementById('nav-menu');
     // Whenever navLink button is clicked it will remove show-menu class
-    // 
+    // to give more space to other section while using mobile devices
     navMenu.classList.remove('show-menu');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// Logic to handle open or close skills
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header');
+console.log(skillsContent);
+console.log(skillsHeader);
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+    // close all the skills section first
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close';
+        console.log(skillsContent[i]);
+    }
+    // Open the one which triggered this function, would
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open';
+    }
+}
+skillsHeader.forEach(el => {
+    el.addEventListener('click', toggleSkills);
+});
