e93c205affa66b61739eb9f24c7687a474b0fbb3
viren sureja <virensureja3@gmail.com>
2023-04-19 20:48:19 +0530

    swiper for portfolio added

--- js/main.js
+++ js/main.js
@@ -79,3 +79,17 @@ modalCloses.forEach(modalClose => {
         });
     });
 });
+
+// portfolio swiper
+let swiper = new Swiper('.mySwiper', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
